2286a8c1b66c931bd14572ff451b0796d3cd0d13
jesus herrada <jesushpalencia.07@gmail.com>
2026-03-06 19:12:32 -0400

    modificacion 2

--- proyecto_so_2026.c
+++ proyecto_so_2026.c
@@ -17,6 +17,7 @@ Producto buffer_descarga[CAP_ZONA_DESCARGA]; //buffer
 int indice_producto = 0; //para desplazarse a traves del buffer 
 pthread_t drones[N_DRONES_PR]; //definicion de un array de 25 drones (hilos)
 //falta definir si es global o local al main ??
+int ids_drones[N_DRONES_PR]; // para almacenar los id de todos los drones
 //definicion de los semaforos de interaccion entre drop_recolector y agente_desifeccion
 sem_t sem_cap_recoleccion; //semaforo contador de dron recolector (25 disponibles)
 pthread_mutex_t sem_sala_desinfeccion; //semaforo para controlar la espera a la sala de desinfeccion , inicializado en 1
@@ -30,7 +31,8 @@ pthread_mutex_t mutex_buffer; //iniciarlizar en 1
 
 
 
-void* dron_recolector(void *arg){
+void* dron_recolector(void *arg){ //el hilo de dron debe tener una idea
+
     while (1){ //garantiza que el dron nunca se detenga mientras el sistema este activo
         //FASE 1: entrada al recinto , solo 25 drones para la zona de cultivo
         int id_dron = *((int*)arg); //convertir el puntero generico a una direccion real int 
@@ -76,17 +78,58 @@ void* dron_recolector(void *arg){
     return NULL;
 
 }
-
+void* agente_desinfeccion(void* arg) {
+    while(1) { //el agente siempre esta operando
+        // El agente espera a que un dron lo llame (Fase 2 del dron)
+        sem_wait(&sem_agente_des); 
+        
+        printf("\n[AGENTE] Recibiendo Dron para desinfección...\n");
+        sleep(2); // Simula el tiempo que tarda el agente trabajando
+        printf("[AGENTE] Dron desinfectado exitosamente.\n");
+        
+        // Avisa al dron que ya terminó su proceso
+        sem_post(&sem_fin_des); 
+    }
+    return NULL;
+}
 
 int main(int argc, char const *argv[]){
     
+    srand(time(NULL));
+    //inicializacion de los semaforos para el proceso dron_recolector
+    sem_init(&sem_cap_recoleccion,0,N_DRONES_PR); //25 capacidad de drones 
+    pthread_mutex_init(&sem_sala_desinfeccion,NULL); //este por defecto se inicializa en 1
+    sem_init(&sem_agente_des,0,0); //inicializado en 0 dado que debe espera que dron_recolector lo llame
+    sem_init(&sem_fin_des,0,0);
+    sem_init(&sem_espacios_vacios,0,CAP_ZONA_DESCARGA); //inicializado en 10 para el buffer
+    sem_init(&sem_elementos_disp,0,0); 
+    pthread_mutex_init(&mutex_buffer,NULL); //para el acceso al buffer , 1 por hilo
 
-    //
-
-
-
-
-
+    pthread_t hilo_agente; ///hilo para el agente
+    pthread_create(&hilo_agente, NULL, agente_desinfeccion, NULL); 
+    //creacion de los hilos para los drones
+    for (int i = 0; i < N_DRONES_PR; i++){
+        ids_drones[i]=i; //asignacion de los ids unicos 
+        pthread_create(&drones[i], NULL, dron_recolector,&ids_drones[i]);
+    }
+    /*
+    for (int i = 0; i < N_DRONES_PR; i++){
+        pthread_join(drones[i],NULL);
+    }
+    
+    //destruccion de los semaforos
+    sem_destroy(&sem_cap_recoleccion);
+    pthread_mutex_destroy(&sem_sala_desinfeccion);
+    sem_destroy(&sem_agente_des);
+    sem_destroy(&sem_fin_des);
+    sem_destroy(&sem_espacios_vacios);
+    sem_destroy(&sem_elementos_disp);
+    pthread_mutex_destroy(&mutex_buffer);
+    */
+    printf("Sistema iniciado. Presiona Ctrl+C para salir.\n");
+    while(1) {
+        sleep(1); 
+    }
 
     return 0;
 }
