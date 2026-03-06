0a47e4b71c57384f80fadc8b3d4b01dc67cbd9ea
jesus herrada <jesushpalencia.07@gmail.com>
2026-03-06 12:44:24 -0400

    definiciones principales

--- proyecto_so_2026.c
+++ proyecto_so_2026.c
@@ -1,25 +1,23 @@
-// Variable global para contabilizar
-int bloqueos_evitados = 0;
-sem_t drones_carga_disponibles; // Inicializado en 4
+#include<stdio.h> //libreria para funciones estandares
+#include<semaphore.h> //para la inclusion de semaforos
+#include<pthread.h> //libreria que proporciona herramientas para manejar hilos , standar POSIX
+#include<unistd.h> //para funciones de interaccion con el sistema operativo, ejemplo sleep (para simular tiempos de espera )
+#define N_DRONES_PR 25
 
-void* hilo_brazo_clasificador(void* arg) {
-    int tipo_producto = (int)arg; 
+pthread_t drones[N_DRONES_PR]; //definicion de un array de 25 drones (hilos)
+//falta definir si es global o local al main ??
 
-    if (tipo_producto == REFRIGERADO) {
-        // Intenta ver si hay 2 drones antes de bloquearse "para siempre"
-        // sem_trywait devuelve -1 si el recurso está ocupado
-        if (sem_trywait(&drones_carga_disponibles) != 0) {
-            printf("LOG: No hay drones suficientes. Bloqueo evitado.\n");
-            bloqueos_evitados++; // <--- AQUÍ LO CONTABILIZAS
-            
-            // Ahora sí, se bloquea formalmente hasta que se liberen
-            sem_wait(&drones_carga_disponibles); 
-        }
-        sem_wait(&drones_carga_disponibles); // Pide el segundo
-        
-        // ... realizar movimiento ...
-        
-        sem_post(&drones_carga_disponibles); // Libera uno
-        sem_post(&drones_carga_disponibles); // Libera el otro
-    }
+
+
+int main(int argc, char const *argv[]){
+    
+
+
+
+
+
+
+
+
+    return 0;
 }
