dcfa6a5f63cb424ad02889121144e85c68abcf05
jesus herrada <jesushpalencia.07@gmail.com>
2026-03-07 17:11:53 -0400

    Guardando cambios temporales en main.h antes de pull

--- main.h
+++ main.h
@@ -11,6 +11,8 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <time.h>
+#include<unistd.h>
+#include<stdio.h>
 
 #define N_DRONES_PR 3
 #define CAP_ZONA_DESCARGA 2
@@ -33,5 +35,20 @@ extern Producto buffer_descarga[CAP_ZONA_DESCARGA];
 extern int indice_producto;
 extern pthread_t drones[N_DRONES_PR];
 extern int ids_drones[N_DRONES_PR];
+extern pthread_mutex_t mutex_buffer_descarga;
+extern sem_t sem_drones_carga; // inicializar en 1
+extern pthread_mutex_t mutex_metricas; 
+extern int bloqueos_evitados;
+extern pthread_mutex_t mutex_buzon;
+extern int buzon_id_brazo;
+extern sem_t sem_iniciar_viaje_dron; 
+extern sem_t sem_fin_viaje_brazo[3];
+extern sem_t sem_plataforma_levitacion;
+extern pthread_mutex_t mutex_metricas_levitacion;
+extern int uso_plataforma_levitacion;
+extern double tiempo_total_acum;
+extern int productos_procesados;
+
+
 
 #endif
