4a99bdecad5701223d55c21edb6d3b3f23b5d38a
ErSamuel <{samuelalejandro2212@gmail.com}>
2026-03-07 16:04:30 -0400

    Añadido mas variables globales

--- main.h
+++ main.h
@@ -14,6 +14,7 @@
 
 #define N_DRONES_PR 3
 #define CAP_ZONA_DESCARGA 2
+#define TOTAL_DEPOSITOS 7
 
 typedef struct {
     int tipo_producto;              //0: Tipo Estandar; 1:Tipo Refrigerado; 2:Tipo Ultra-Procesado
@@ -28,6 +29,12 @@ extern sem_t sem_agente_des;                        //Despierta al agente de des
 extern sem_t sem_fin_des;                           //Semaforo donde el Dron recolector espera hasta ser desinfectado
 extern sem_t sem_espacios_vacios;                   //Limita la entrada a la zona de descarga
 extern sem_t sem_elementos_disp;                    //Llama al proceso del brazo. Indica que dejo elementos en la zona de descarga
+extern sem_t sem_plataforma;                        //Semaforo que limita el uso de la PLataforma Magnetica
+
+extern int deposito[TOTAL_DEPOSITOS];               //Vector que almacena la cantidad de cajas por deposito. 0-3:Estandar; 4-6:Refrigerado; 7:Ultra-Procesado
+extern int indice_deposito_estandar;                //Lleva el indice del vector deposito en la seccion de productos Estandar
+extern int indice_deposito_refrigerado;             //Lleva el indice del vector deposito en la seccion de productos Refrigerados
+
 extern pthread_mutex_t mutex_buffer;                //permite la modificacion en la estructura que almacena los elementos de la zona de descarga
 extern Producto buffer_descarga[CAP_ZONA_DESCARGA]; //[Zona de descarga], buffer donde se almacenaran los productos
 extern int indice_producto;                         //lleva el conteo de cuantos productos hay
