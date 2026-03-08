c2883047018726af08d612a3cb6021479a3f655b
suswolf <jesus.a.munoz.m@gmail.com>
2026-03-08 11:38:51 -0400

    Cambio en los comments

--- main.c
+++ main.c
@@ -46,10 +46,10 @@ int productos_procesados=0;                         //productos totales procesad
 const char tipo_producto_str[3][20] = {"Estándar", "Refrigerado", "Ultradelicado"}; 
                                                     //para imprimir el tipo de producto en texto
 int frecuencias_pr[4][12] = {
-    {0,0,0,0,1,1,1,1,2,2,2,2}, // Frecuencias normales
-    {0,0,0,0,0,0,1,1,1,2,2,2},   // Frecuencias favorables para productos estandar
-    {1,1,1,1,1,1,0,0,0,2,2,2},    // Frecuencias favorables para productos refrigerados
-    {2,2,2,2,2,2,0,0,0,1,1,1}      // Frecuencias favorables para productos ultradelicados
+    {0,0,0,0,1,1,1,1,2,2,2,2},                      // Frecuencias normales
+    {0,0,0,0,0,0,1,1,1,2,2,2},                      // Frecuencias favorables para productos estandar
+    {1,1,1,1,1,1,0,0,0,2,2,2},                      // Frecuencias favorables para productos refrigerados
+    {2,2,2,2,2,2,0,0,0,1,1,1}                       // Frecuencias favorables para productos ultradelicados
 };
 int selector_frecuencia = 0; // Índice para seleccionar la frecuencia de generación de productos 
 
